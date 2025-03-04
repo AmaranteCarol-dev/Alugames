c4b489735d6a846f724582fba2457dd7736f4e23
Carol <carol.45724@gmail.com>
2025-03-04 13:22:13 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+let jogosAlugados = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
+    }
+    contarEExibirJogosAlugados();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
+
+// Função para verificar se uma palavra ou frase é um palíndromo
+function ehPalindromo(texto) {
+    let textoFormatado = texto.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let textoInvertido = textoFormatado.split('').reverse().join('');
+    return textoFormatado === textoInvertido;
+}
+
+console.log(ehPalindromo("A base do teto desaba")); // true
+console.log(ehPalindromo("Hello")); // false
+
+// Função para ordenar três números em ordem crescente
+function ordenarNumeros(a, b, c) {
+    const numerosOrdenados = [a, b, c].sort((x, y) => x - y);
+    console.log(`Números ordenados: ${numerosOrdenados.join(', ')}`);
+}
+
+// Exemplo de uso:
+ordenarNumeros(3, 1, 5); // Deve exibir "Números ordenados: 1, 3, 5"
